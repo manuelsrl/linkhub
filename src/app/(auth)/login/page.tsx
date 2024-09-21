25b146693de610e5edcc79df0238993fd45861a8
manuelsrl <manuelschallerrosal@gmail.com>
2024-09-20 21:00:46 -0300

    Shadcn form added, button outline variant changed and added transition-color on card ui component

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -8,6 +8,11 @@ import {
   CardContent,
 } from "@/components/ui/card";
 import { FullLogo } from "@/components/FullLogo";
+import { FcGoogle } from "react-icons/fc";
+import { IoLogoGithub } from "react-icons/io";
+import { Button } from "@/components/ui/button";
+
+import Link from "next/link";
 
 const LoginPage = () => {
   return (
@@ -17,18 +22,33 @@ const LoginPage = () => {
 
         <main className="flex flex-grow items-center justify-center py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6">
           <div className="max-w-xl lg:max-w-3xl">
-            <FullLogo />
+            <Link href="/">
+              <FullLogo />
+            </Link>
 
-            <Card className="bg-transparent dark:bg-transparent mx-20 mt-10">
+            <Card className="bg-transparent dark:bg-transparent mt-10">
               <CardHeader>
-                <CardTitle className="text-3xl">Welcome back</CardTitle>
-                <CardDescription>Log in to your LinkHub</CardDescription>
+                <CardTitle className="text-3xl text-center">
+                  Welcome back
+                </CardTitle>
+                <CardDescription className="text-md text-center">
+                  Log in to your LinkHub
+                </CardDescription>
               </CardHeader>
               <CardContent>
                 <LoginForm />
               </CardContent>
               <CardFooter>
-                <p>Card Footer</p>
+                <div className="flex flex-col gap-4 sm:flex-row sm:gap-6">
+                  <Button variant="outline">
+                    <FcGoogle size={20} />
+                    Sign in with Google
+                  </Button>
+                  <Button variant="outline">
+                    <IoLogoGithub size={20} />
+                    Sign in with Github
+                  </Button>
+                </div>
               </CardFooter>
             </Card>
           </div>
